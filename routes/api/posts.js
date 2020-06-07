dfa06a4e4d6c6ff5c6f9302d566972c1edd8ebdc
tylertownsend <tyler.townsend.js@gmail.com>
2020-06-07 16:02:39 -0700

    Add post for user

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const auth = require('../../middleware/auth');
+
+const Post = require('../../models/post');
+const User = require('../../models/users');
 
 /**
  * @route       GET api/post
@@ -8,6 +13,32 @@ const router = express.Router();
  */
 router.get('/', (req, res) => {
   res.send('Post router');
+});
+
+router.post('/', [ auth, [
+  check('text', 'Text is required').not().isEmpty()
+]], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const user = await User.findById(req.user.id).select('-password');
+
+  try {
+    const newPost = new Post({
+      text: req.body.text,
+      name: user.name,
+      avatar: user.avatar,
+      user: req.user.id
+    });
+
+    const post = await newPost.save();
+    res.json(post);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Send error');
+  }
 });
 
 module.exports = router;
