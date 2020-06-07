0b2de855be419db65618f8b3935e3729f1cac14b
tylertownsend <tyler.townsend.js@gmail.com>
2020-06-07 14:01:36 -0700

    Functionality for add/remove education

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -232,4 +232,75 @@ router.delete('/experience/:exp_id', auth, async (req, res) => {
   }
 });
 
+/**
+ * @route       PUT api/profile/education
+ * @description Add profile education
+ * @access      Private
+ */
+router.put('/education', [auth, [
+  check('school', 'School is required').not().isEmpty(),
+  check('degree', 'Degree is required').not().isEmpty(),
+  check('fieldOfStudy', 'Field of study is required').not().isEmpty(),
+  check('from', 'From Date is required').not().isEmpty()
+]], async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const {
+      school,
+      degree,
+      fieldOfStudy,
+      location,
+      from,
+      to,
+      current,
+      description
+    } = req.body;
+
+    const newEdu = {
+      school,
+      degree,
+      fieldOfStudy,
+      from,
+      to,
+      current,
+      description
+    }
+  try { 
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    profile.education.unshift(newEdu);
+
+    await profile.save();
+    res.json(profile);
+  } catch(err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+/**
+ * @route       DELETE api/profile/education/:edu_id
+ * @description Delete education from profile
+ * @access      Private
+ */
+router.delete('/education/:exp_id', auth, async (req, res) => {
+  try { 
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    const removeIndex = profile.education.map(item => item.id).indexOf(req.params.exp_id);
+
+    profile.education.splice(removeIndex, 1);
+
+    await profile.save();
+    res.json(profile);
+  } catch(err) {
+    console.error(err.message);
+    if (err.kind == 'ObjectId') {
+       return res.status(400).json({ msg: 'Profile not found' });
+    }
+    res.status(500).send('Server Error');
+  }
+});
 module.exports = router;
