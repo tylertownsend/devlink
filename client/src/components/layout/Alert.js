3acd884c7b8137cf9d367b81a8d08a1d7bebf6bc
tylertownsend <tyler.townsend.js@gmail.com>
2020-06-15 23:25:24 -0700

    Added Alert class

---
+++ client/src/components/layout/Alert.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux'
+
+export const Alert = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default connect()(Alert);
