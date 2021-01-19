374068d84c7d81a359b52e59263b66abd50cf8b9
Tyler Townsend <townsend.ts@outlook.com>
2021-01-18 17:15:07 -0800

    Profile action/reducer

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './client/css/style.css';
+import PrivateRoute from './components/routing/PrivateRoute';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
-import './client/css/style.css';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
+import Dashboard from './components/dashboard/Dashboard';
 
 // Redux
 import { Provider } from 'react-redux';
@@ -33,6 +35,7 @@ const App = () => {
             <Switch>
               <Route exact path='/register' component={Register}/>
               <Route exact path='/login' component={Login}/>
+              <PrivateRoute exact path='/dashboard' component={Dashboard}/>
             </Switch>
           </section>
         </Fragment>
