9bfad21c2d887bd450315fb389554f14cc3f0df8
Tyler Townsend <townsend.ts@outlook.com>
2021-02-28 23:44:24 -0800

    Updated private route to typescript

--- client/src/components/routing/PrivateRoute.tsx
+++ client/src/components/routing/PrivateRoute.tsx
@@ -1,13 +1,39 @@
-import React from 'react';
+import * as React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import { connect, ConnectedProps } from 'react-redux';
 
-const PrivateRoute = (content: any) => {
+type RootState = {
+  auth: {
+    isAuthenticated: boolean;
+    loading: boolean;
+  }
+} 
+
+function mapStateToProps(state: RootState) {
+  return {
+    auth: state.auth
+  };
+};
+
+const connector = connect(
+  mapStateToProps
+)
+
+type PropsFromRedux = ConnectedProps<typeof connector>
+
+interface Props extends PropsFromRedux {
+  component: any;
+  exact: boolean;
+  path: string;
+}
+
+const PrivateRoute = (content: Props) => {
   const { component: Component,
     auth: { isAuthenticated, loading },
     ...rest 
   } = content;
+  console.log(Component);
   return(
     <Route {...rest} render={props => !isAuthenticated && !loading ? (<Redirect to='/login'/>) :( <Component />)}/>
   );
@@ -17,8 +43,6 @@ const PrivateRoute = (content: any) => {
 //   auth: PropTypes.object.isRequired
 // }
 
-const mapStateToProps = (state: any) => ({
-  auth: state.auth
-});
 
-export default connect(mapStateToProps)(PrivateRoute);
+
+export default connector(PrivateRoute);
